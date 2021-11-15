3648f76df8c1394cad93dfcd4cbecdff9c242f9a
Leo Andersson Guillou <leo.anderssonguillou@elev.ga.ntig.se>
2021-11-15 13:27:43 +0100

    First

---
+++ Slots_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slots_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
