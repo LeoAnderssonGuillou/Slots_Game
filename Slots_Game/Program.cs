7ba35f36ef3695e9018df15d0996036396f46b3b
Leo Andersson Guillou <leo.anderssonguillou@elev.ga.ntig.se>
2021-11-18 09:56:58 +0100

    Basic Window

--- Slots_Game/Program.cs
+++ Slots_Game/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace Slots_Game
 {
@@ -7,6 +10,27 @@ namespace Slots_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Vector2 winSize = new Vector2(1920, 1200);
+            Raylib.InitWindow((int)winSize.X, (int)winSize.Y, "Gymnasiearbete");
+            Raylib.SetTargetFPS(165);
+
+
+             while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+                float delta = Raylib.GetFrameTime();
+
+
+
+
+
+                //FPS
+                int fps = Raylib.GetFPS();
+                Raylib.DrawText($"{fps}", 50, 50, 50, Color.GRAY);
+
+                Raylib.EndDrawing();
+            }
         }
     }
 }
